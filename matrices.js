92bc02787d799198893ebdb3eafe91dacd7caf96
Felix Gers <gers@beuth-hochschule.de>
2011-05-17 17:02:06 +0200

    The new version with strict object, but the tick does not work.

--- matrices.js
+++ matrices.js
@@ -9,5 +9,20 @@ function Matrices() {
 	this.pMatrix = mat4.create();
 	// Model-View matrix.
 	this.mvMatrix = mat4.create();
+	// Matrix stack for mvMatrix.
+	this.mvMatrixStack = [];
+
+	this.mvPushMatrix = function() {
+		var copy = mat4.create();
+		mat4.set(this.mvMatrix, copy);
+		this.mvMatrixStack.push(copy);
+	};
+
+	this.mvPopMatrix =  function() {
+		if (this.mvMatrixStack.length == 0) {
+			throw "Invalid popMatrix!";
+		}
+		this.mvMatrix = this.mvMatrixStack.pop();
+	};
 }
 
