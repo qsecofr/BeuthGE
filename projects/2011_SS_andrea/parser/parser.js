c9240e86c925c0067755cf91d6a6f7ccc594fc4a
Andrea Kuschan <Andrea@.(none)>
2011-05-25 22:14:11 +0200

    change parser

---
+++ projects/2011_SS_andrea/parser/parser.js
@@ -0,0 +1,154 @@
+function parse(data){
+	var parser=new ColladaParser();
+	parser.parseCollada(data);
+}
+
+
+
+
+var ColladaParser=function(){
+	this.parser = new DOMImplementation();
+	
+	this.parseCollada=function(data){
+		//create xmldocument 
+		var domDoc = this.parser.loadXML(data.value);
+		//getting rootnode
+		var docRoot = domDoc.getDocumentElement();
+		var geometryNode=docRoot.selectNodeSet("//library_geometries/geometry/mesh");
+		var allOffsets=new Array();
+		//step 1 read VERTEX ******************************************************************************
+		
+		var node = geometryNode.item(0).selectNodeSet("//polylist/input[@semantic=VERTEX]");
+		//read vertexOffset
+		var vertexOffset=node.item(0).getAttributes().getNamedItem("offset").getNodeValue();
+		allOffsets.push(vertexOffset);
+		//read vertices nodename 
+		var vertexId=node.item(0).getAttributes().getNamedItem("source").getNodeValue();
+		//remove '#'
+		vertexId=this.substringSign(vertexId,'#');
+		//getting node vertices
+		node=geometryNode.item(0).selectNodeSet("//vertices[@id=" + vertexId+ "]");
+		node=node.item(0).getFirstChild();
+		//read id of position-array
+		vertexId=node.getAttributes().getNamedItem("source").getNodeValue();
+		//remove '#'
+		vertexId=this.substringSign(vertexId,'#');
+		
+		//step 2 read NORMALS *******************************************************************************
+		
+		//read normalid
+		node = geometryNode.item(0).selectNodeSet("//polylist/input[@semantic=NORMAL]");
+		//read normalOffset
+		var normalOffset=node.item(0).getAttributes().getNamedItem("offset").getNodeValue();
+		allOffsets.push(normalOffset);
+		var normalId=node.item(0).getAttributes().getNamedItem("source").getNodeValue();
+		//remove '#'
+		normalId=this.substringSign(normalId,'#');
+		
+		
+		//step 3 read sources *******************************************************************************
+		
+		var sources=docRoot.selectNodeSet("//source");
+		for(i=0;i<sources.length;i++){
+          node=sources.item(i);    
+		  nodeId=node.getAttributes().getNamedItem("id").getNodeValue();	
+		  //read accessor
+			tempNode=node.selectNodeSet("//technique_common/accessor");
+			source=tempNode.item(0).getAttributes().getNamedItem("source").getNodeValue();
+		  
+		  if(nodeId==vertexId){
+			  //node with vertices found
+			  sourceVerticeName=nodeId;
+			  source=this.substringSign(source,"#");
+			  tempNode=node.selectNodeSet("//float_array[@id=" + source + "]");
+			  vertices=tempNode.item(0).getFirstChild().getNodeValue();
+			  
+			  //replace whitespaces with 
+			  vertices=vertices.split(' ');
+			}else if (nodeId==normalId){
+			    sourceNormalsName=nodeId;
+				//read normals
+				//TODO auslagern
+				source=this.substringSign(source,"#");
+				tempNode=node.selectNodeSet("//float_array[@id=" + source + "]");
+				normals=tempNode.item(0).getFirstChild().getNodeValue();
+				//replace whitespaces with 
+				normals=normals.split(' ');
+			}else{
+				alert("unknown source found");
+			}			
+		}
+		
+		//step 4 read indicies *****************************************************************************
+		var vertexIndicies;
+		var normalsIndicies;
+		
+		//read polylist node p
+		node= geometryNode.item(0).selectNodeSet("//polylist/p");
+		var indicies=node.item(0).getFirstChild().getNodeValue();
+		//replace whitespaces with 
+		indicies=indicies.split(' ');
+		allOffsets.sort(this.numSort);
+		
+		//iterate thr. indicies
+		var offset=allOffsets[0];
+		var vertexSortedArray=new Array();
+		var normalsSortedArray=new Array();
+		var indiciesForWeb=new Array();
+		
+		do{
+			
+			if(offset==vertexOffset){
+				//get vertexentry for this indicies				
+				indiciesForWeb.push(indicies[0]);
+			}else if (offset==normalOffset){
+				//normalsSortedArray.push(normals[indicies[i]]);
+			}else{
+				alert("unknown offset");
+			}
+			
+			//set next offset
+			if(offset==allOffsets.length-1){
+				offset=allOffsets[0];
+			}else{
+				offset++;
+			}
+			
+			//remove first indicies-part
+			indicies.shift();
+		}while(indicies.length>0);
+		
+		for(var i=0;i<vertexSortedArray.length;i++){
+			indiciesForWeb.push(i);
+		}
+		
+		var JSObject={
+			"cube":{
+				"v":vertices,
+				"n":normals,
+				"i":indiciesForWeb,
+			}
+		};
+		
+		// Das Objekt zu JSON kodieren
+		var jsonCode = JSON.stringify(JSObject);
+		document.write(jsonCode);
+		
+	}
+	
+	this.numSort = function(a,b){
+	   return a - b;
+	}
+	
+	this.substringSign = function(str,sign){
+		posi=str.indexOf(sign);
+		if(posi!= -1){
+			return str.substring(posi + 1 ,str.length);
+		}
+	}
+
+	this.parseGeometry = function(geometryXML){
+		alert(geometryXML);
+	}
+}
+    
